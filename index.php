dedfcf39f77e70ad69b574ab09c2616bed490b0e
Алексей Иванов <Alessio18911@gmail.com>
2019-03-20 23:09:00 +0300

    Правки по задаче переход на главную при попытке ввести из браузера несуществующую категорию

--- index.php
+++ index.php
@@ -1,15 +1,24 @@
 <?php
 
-require_once 'parts/header.php';
+require_once 'db/db.php';
 
 if(isset($_GET['cat'])) {
     $currentCat = $_GET['cat'];
     $products = $connect->query("SELECT * FROM products WHERE cat = '$currentCat'");
+
+    if(!$products->rowCount()) {
+        header("Location: /");
+        exit();
+    }
+
     $products = $products->fetchAll(PDO::FETCH_ASSOC);
+
 } else {
     $products = $connect->query("SELECT * FROM products");
     $products = $products->fetchAll(PDO::FETCH_ASSOC);
 }
+
+require_once 'parts/header.php';
 
 ?>
 
