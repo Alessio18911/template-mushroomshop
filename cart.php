7ef4a6b13ed1abf6bbcbc60685e423e3f45fb4b7
Алексей Иванов <Alessio18911@gmail.com>
2019-03-21 14:45:37 +0300

    Переход из корзины по карточку товара

--- cart.php
+++ cart.php
@@ -10,7 +10,9 @@ foreach($_SESSION['cart'] as $product) {
 ?>
 
 <div class="cart">
-    <img src="img/<?= $product['img'] ?>" alt="<?= $product['rus_name'] ?>">
+    <a href="product.php?product=<?=$product['title']?>">
+        <img src="img/<?= $product['img'] ?>" alt="<?= $product['rus_name'] ?>">
+    </a>
     <div class="cart-descr">
         <?= $product['rus_name'] ?> в количестве <?= $product['quantity'] ?> шт на сумму <?= $product['quantity'] * $product['price'] ?> рублей
     </div>
